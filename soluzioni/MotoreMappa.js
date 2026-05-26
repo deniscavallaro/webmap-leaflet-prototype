5a83a3a9c99b030e8fa9e13c8e97f8483f48269b
Denis Cavallaro <denis.cavallaro@brainsix.it>
2026-05-26 14:42:13 +0200

    Soluzione 2 del 26/05/2026

--- soluzioni/MotoreMappa.js
+++ soluzioni/MotoreMappa.js
@@ -13,8 +13,32 @@ export class MotoreMappa {
             L.marker([45.407733, 11.873339]).bindPopup("Punto 1"),
             L.marker([45.408500, 11.875000]).bindPopup("Punto 2")
         ]);
+
+		var Bbox_width= 18.99-5.93;
+		var startResolution = Bbox_width/1024;
+		var grid_resolution = new Array(22);
+		for (var i = 0; i < 22; ++i) {
+			grid_resolution[i] = startResolution / Math.pow(2, i);
+		}       
+		var crs_6706 = new L.Proj.CRS('EPSG:6706', '+proj=longlat +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +no_defs',
+		   {
+			 origin: [0, 0],
+			 bounds: L.bounds([5.93, 34.76], [18.99, 47.1]),
+			 resolutions: grid_resolution
+		   });       
+		const wmsLayer_1 = L.tileLayer.wms('https://wms.cartografia.agenziaentrate.gov.it/inspire/wms/ows01.php', {
+			layers: ['province', 'CP.CadastralZoning'],
+			crs: crs_6706,
+			format: 'image/png',
+			maxZoom: 19,
+			transparent: true,
+			attribution: '© ' + '<a href="https://creativecommons.org/licenses/by-nc-nd/2.0/it/">Agenzia delle Entrate</a>',
+		});
+	
         this.layers["punti"] = punti;
+        this.layers["province"] = wmsLayer_1;
         punti.addTo(this.map);
+        wmsLayer_1.addTo(this.map);
     }
 
     toggleLayer(nomeLayer, stato) {
