a0ed711399b36b04c762e0f2be4c5b7ed7e7d5c4
Matilde <b6.stage.2026.1@gmail.com>
2026-06-04 17:30:07 +0200

    ottavo commit - 4.06.2026

--- MotoreMappa.js
+++ MotoreMappa.js
@@ -13,7 +13,7 @@ export class MotoreMappa {
     };
 
     /**
-	 * Inizializza e gestisce una mappa Leaflet ei suoi layers
+	 * Inizializza e gestisce una mappa Leaflet 
      */
     
     iniziomappa(){
@@ -23,11 +23,22 @@ export class MotoreMappa {
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);*/
 
-        /*  
-            basemap -> stradale, ortofoto, oceani, rilievi
-            overlay -> province, punti 
-        */
+        // per non creare problemi 
+        this.map.doubleClickZoom.disable();
 
+        this.aggiungiStrumenti();
+    }
+
+    //---------------------------------------------------
+    //              BASEMAP,LAYER, BOTTONI
+    //---------------------------------------------------
+
+
+    /**
+	 * Crea i layer predefiniti e li attiva tutti  
+	 *           -> province, punti
+     */
+    creaLayerTematici(){
         const m1 = L.marker([45.26, 12.19]).bindPopup('questo è il marker 1'), // venezia  
               m2 = L.marker([45.407733, 11.873339]).bindPopup('questo è il marker 2'), // padova
               m3 = L.marker([45.50, 12.30]).bindPopup('questo è il marker 3')
@@ -63,105 +74,20 @@ export class MotoreMappa {
         
         // visualizzo layers in console
         console.log(this.layers)
+    }
 
-        // attivazione automatica layers
-        for(const nomeLayer of this.layerAttivi){
-            const ll = this.layers[nomeLayer]
-            if(ll) ll.addTo(this.map)
+    /**
+	 * Attiva in automatico i layer
+     */
+    accendiLayerIniziali() {
+        for (const nomeLayer of this.layerAttivi) {
+            this.toggleLayer(nomeLayer, true);
         }
-
-        this.creaBaseMaps();
-        this.creaLayerControl(); 
-    }
-
-    /**
-	 * Attiva o disattiva uno o più layer 
-	 * 
-	 * @param string nomeLayer -> nome del layer
-	 * @param boolean visibile -> se true il layer è attivo, altrimenti non lo è
-     */
-    toggleLayer(nomeLayer, visibile){
-       const layer = this.layers[nomeLayer];
-       //se la mappa non contiene il layer
-		if (!layer) {
-			console.log("Layer inesistente -> " + nomeLayer);
-			return;
-		}
-		if (visibile) {
-			this.map.addLayer(layer);
-		} else {
-			this.map.removeLayer(layer);
-		}
-    }
-
-    /**
-	 * Attiva o disattiva tutti i layers contemporaneamente
-	 * 
-	 * @param boolean visibile -> se true il layer è attivo, altrimenti non lo è
-     */
-
-    toggleTutti(visibile){
-		for (const nomeLayer in this.layers) {
-			this.toggleLayer(nomeLayer, visibile);
-		}
-    }
-
-    /**
-	 * Gestisce i bottoni che attivano o disattivano i layers 
-	 * 
-     */
-    buttons() {
-        const btnpunti = document.getElementById("btnpunti");
-        const btnprovince = document.getElementById("btnprovince");
-        const btntutti = document.getElementById("btntutti");
-
-		let puntiVisibili = this.layerAttivi.includes("punti");
-		let provinceVisibili = this.layerAttivi.includes("province");
-		let tuttoVisibile = puntiVisibili && provinceVisibili;
-		
-		this.aggiornaTestiBottoni(btnpunti, btnprovince, btntutti, puntiVisibili, provinceVisibili, tuttoVisibile);
-        
-        btnpunti.addEventListener("click", () => {
-            puntiVisibili = !puntiVisibili;
-            this.toggleLayer("punti", puntiVisibili);
-            btnpunti.textContent = (puntiVisibili ? "Spegni punti" : "Accendi punti");
-        });
-
-        btnprovince.addEventListener("click", () => {
-            provinceVisibili = !provinceVisibili;
-            this.toggleLayer("province", provinceVisibili);
-            btnprovince.textContent = (provinceVisibili ? "Spegni province" : "Accendi province");
-        });
-
-        btntutti.addEventListener("click", () => {
-            tuttoVisibile = !tuttoVisibile;
-            this.toggleTutti(tuttoVisibile);
-            puntiVisibili = tuttoVisibile;
-            provinceVisibili = tuttoVisibile;
-            this.aggiornaTestiBottoni(btnpunti, btnprovince, btntutti, puntiVisibili, provinceVisibili, tuttoVisibile);
-        });
-    }
-
-    /**
-	 * Aggiorna i testi dei bottoni
-	 * 
-	 * @param button btnpunti    -> bottone che attiva/disattiva marker
-     * @param button btnprovince -> bottone che attiva/disattiva layer province
-     * @param button btntutti    -> bottone che attiva/disattiva tutt i layers
-     * 
-     * @param boolean puntiVisibili     -> true se il layer "punti" è attivo, false se non lo è
-     * @param boolean provinceVisibili  -> true se il layer "province" è attivo, false se non lo è
-     * @param boolean tuttoVisibile     -> true se tutti i layers sono attivi, false se non lo sono
-     */
-    aggiornaTestiBottoni( btnpunti, btnprovince, btntutti, puntiVisibili, provinceVisibili, tuttoVisibile){
-        btnpunti.textContent = (puntiVisibili ? "Spegni punti" : "Accendi punti");
-        btnprovince.textContent = (provinceVisibili ? "Spegni province" : "Accendi province");
-        btntutti.textContent = (tuttoVisibile ? "Spegni tutti i layers" : "Accendi tutti i layers");
     }
 
     /**
 	 * Crea le basemap predefinite e attiva quella di default 
-	 * 
+	 *           -> openstreetmap (attivata), stradale, ortofoto, oceani, rilievi
      */
     creaBaseMaps(){
         //basemap openstreet map, di base
@@ -218,12 +144,11 @@ export class MotoreMappa {
         this.aggiungiBaseMap("<span style='color: brown'> Rilievi </span>", rilievi);
 
         //aggiungo la basemap predefinita alla mappa
-        this.basemaps[this.nomeBasemapPredefinita].addTo(this.map);
+        this.basemaps[this.nomeBasemapPredefinita].addTo(this.map); 
     }
 
     /**
-	 * Crea il pannello di controlllo Leaflet per le basemap e i layer
-	 * 
+	 * Crea il pannello di controlllo Leaflet per le basemap e i layer 
      */
     creaLayerControl(){
         var layerControl = L.control.layers(this.basemaps, this.layers).addTo(this.map);
@@ -234,12 +159,200 @@ export class MotoreMappa {
 	 * 
 	 * @param string nome         -> nome visualizzato della basemap
      * @param var layer           -> nome della variabile legata alla basemap
-	 * @param boolean predefinita -> se true la basemap viene inportata come predefinita, altrimenti niente
+	 * @param boolean predefinita -> se true la basemap viene inportata come predefinita, altrimenti niente  
      */
     aggiungiBaseMap(nome, layer, predefinita = false){
         this.basemaps[nome]= layer;
         if(predefinita) this.nomeBasemapPredefinita = nome;
     }
+
+    /**
+	 * Attiva o disattiva uno o più layer 
+	 * 
+	 * @param string nomeLayer -> nome del layer
+	 * @param boolean visibile -> se true il layer è attivo, altrimenti non lo è
+     */
+    toggleLayer(nomeLayer, visibile){
+       const layer = this.layers[nomeLayer];
+       //se la mappa non contiene il layer
+		if (!layer) {
+			console.log("Layer inesistente -> " + nomeLayer);
+			return;
+		}
+		if (visibile) {
+			this.map.addLayer(layer);
+		} else {
+			this.map.removeLayer(layer);
+		}
+    }
+
+    /**
+	 * Attiva o disattiva tutti i layers contemporaneamente
+	 * 
+	 * @param boolean visibile -> se true il layer è attivo, altrimenti non lo è
+     */
+
+    toggleTutti(visibile){
+		for (const nomeLayer in this.layers) {
+			this.toggleLayer(nomeLayer, visibile);
+		}
+    }
+
+    /**
+	 * Gestisce i bottoni che attivano o disattivano i layers 
+	 * 
+     */
+    buttons() {
+        const btnpunti = document.getElementById("btnpunti");
+        const btnprovince = document.getElementById("btnprovince");
+        const btntutti = document.getElementById("btntutti");
+
+		let puntiVisibili = this.layerAttivi.includes("punti");
+		let provinceVisibili = this.layerAttivi.includes("province");
+		let tuttoVisibile = puntiVisibili && provinceVisibili;
+		
+		this.aggiornaTestiBottoni(btnpunti, btnprovince, btntutti, puntiVisibili, provinceVisibili, tuttoVisibile);
+        
+        btnpunti.addEventListener("click", () => {
+            puntiVisibili = !puntiVisibili;
+            this.toggleLayer("punti", puntiVisibili);
+            btnpunti.textContent = (puntiVisibili ? "Spegni punti" : "Accendi punti");
+        });
+        btnprovince.addEventListener("click", () => {
+            provinceVisibili = !provinceVisibili;
+            this.toggleLayer("province", provinceVisibili);
+            btnprovince.textContent = (provinceVisibili ? "Spegni province" : "Accendi province");
+        });
+        btntutti.addEventListener("click", () => {
+            tuttoVisibile = !tuttoVisibile;
+            this.toggleTutti(tuttoVisibile);
+            puntiVisibili = tuttoVisibile;
+            provinceVisibili = tuttoVisibile;
+            this.aggiornaTestiBottoni(btnpunti, btnprovince, btntutti, puntiVisibili, provinceVisibili, tuttoVisibile);
+        });
+    }
+ 
+    /**
+	 * Aggiorna i testi dei bottoni
+	 * 
+	 * @param button btnpunti    -> bottone che attiva/disattiva marker
+     * @param button btnprovince -> bottone che attiva/disattiva layer province
+     * @param button btntutti    -> bottone che attiva/disattiva tutt i layers
+     * 
+     * @param boolean puntiVisibili     -> true se il layer "punti" è attivo, false se non lo è
+     * @param boolean provinceVisibili  -> true se il layer "province" è attivo, false se non lo è
+     * @param boolean tuttoVisibile     -> true se tutti i layers sono attivi, false se non lo sono
+     */
+    aggiornaTestiBottoni( btnpunti, btnprovince, btntutti, puntiVisibili, provinceVisibili, tuttoVisibile){
+        btnpunti.textContent = (puntiVisibili ? "Spegni punti" : "Accendi punti");
+        btnprovince.textContent = (provinceVisibili ? "Spegni province" : "Accendi province");
+        btntutti.textContent = (tuttoVisibile ? "Spegni tutti i layers" : "Accendi tutti i layers");
+    };
+
+
+    //---------------------------------------------------
+    //                     STRUMENTI
+    //---------------------------------------------------
+
+
+    /**
+	 * Scala della mappa
+     */
+    aggiungiScala(){
+        L.control.scale({ 
+            position: "bottomleft", 
+            metric: true,
+            imperial: false
+        }).addTo(this.map);
+    }
+
+    /**
+	 * Coordinate sempre visibili
+     */
+    aggiungiPannelloCoordinate(){
+        this.pannelloCoordinate = L.control({ position: "bottomright"});
+
+        this.pannelloCoordinate.onAdd = function(){
+            const div = L.DomUtil.create("div", "pannello-coordinate");
+            div.innerHTML = "Lat: - , Lng: - ";
+            return div;
+        };
+
+        this.pannelloCoordinate.addTo(this.map);
+
+        this.map.on("mousemove", (evento) => {
+            const lat = parseFloat(evento.latlng.lan);             // not a number
+            const lng = parseFloat(evento.latlng.lng);
+            const div = document.querySelector(".pannello-coordinate");
+            div.innerHTML = "Lat: " + lat + " | Lng: "+ lng;
+        });
+    }
+
+    /**
+	 * Al dopppio click da le coordinate
+     */
+    aggiungiDoubleClickCoordinate(){
+        this.map.on("dbclick", (evento) => {
+            const lat = parseFloat(evento.latlng.lan);             // not a number
+            const lng = parseFloat(evento.latlng.lng);
+            L.popup().setLatLng(evento.latlng)
+                    .setContent("Lat: " + lat + " <br>Lng: "+ lng)
+                    .openOn(this.map);
+        });
+    };
+
+    /**
+	 * Aggiunge la barra di ricerca
+     */
+    aggiungiRicerca(){
+        L.Control.geocoder({
+            defaultMarkGeocode: true,
+            placeholder: "Cerca un luogo..."
+        }).addTo(this.map);
+    }
+
+    /**
+	 * Minimappa!
+     */
+    aggiungiMinimappa(){
+        const miniLayer = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+                maxZoom: 19,
+                attribution: '© OpenStreetMap'
+            });
+        new L.Control.MiniMap(miniLayer, {
+            position: "bottomleft",
+            toggleDisplay: true,
+            minimized: false
+        }).addTo(this.map);
+    }
+    
+    /**
+	 * Aggiunge in un colpo solo tutti gli strumenti al metodo di inizializzazione della mappa
+     */
+    aggiungiStrumenti(){
+        this.creaBaseMaps();
+        this.creaLayerTematici();
+        this.accendiLayerIniziali();
+        this.creaLayerControl();
+        this.aggiungiScala();
+        this.aggiungiPannelloCoordinate();
+        this.aggiungiDoubleClickCoordinate();
+        this.aggiungiRicerca();
+        this.aggiungiMinimappa();
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     /**
 	 * (dovrebbe) creare una nuova basemap con delle info standard
@@ -259,13 +372,8 @@ export class MotoreMappa {
                 label: label,
                 maxZoom: maxZoom,
                 attribution: attribution
-            })
+            });
         }else{console.log("Layer esistente");}
     }
         
-
-    }
-
-
-
-  
+}
